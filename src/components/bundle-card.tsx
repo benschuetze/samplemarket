a3e8b274f2fb83767a24b1221e0817a04c14a929
Benjamin <benschuetze@gmx.de>
2024-04-02 19:11:34 +0200

    fixed button logic

--- src/components/bundle-card.tsx
+++ src/components/bundle-card.tsx
@@ -1,8 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 import { supabase } from "../supabase";
 import { useEffect, useState } from "react";
 import { ScrollArea } from "./ui/scroll-area";
+import { DockIcon, PlusIcon } from "lucide-react";
 
 export const SampleBundle = () => {
   // later this will all be rewritten so that i dont request the stored files in the bucket but the
@@ -10,6 +12,8 @@ export const SampleBundle = () => {
   // pass it as a prop
 
   const [fileUrls, setFileUrls] = useState<string[]>([]);
+  const [idOfCurrentlyHoveredSample, setIdOfCurrentlyHoveredSample] =
+    useState<string>("");
   const getAudio = async () => {
     const { data, error } = await supabase.storage
       .from("sounds-for-development")
@@ -22,20 +26,26 @@ export const SampleBundle = () => {
         },
       });
     if (error) console.log("error: ", error);
-    console.log("data: ", data);
     const fileUrls: Array<string> = [];
     data?.forEach((fileData) => {
       const name = fileData.name;
       const { data } = supabase.storage
         .from("sounds-for-development")
         .getPublicUrl(`samples/${name}`);
-      console.log("file url: ", data);
       fileUrls.push(data.publicUrl);
     });
     setFileUrls((prev) => fileUrls);
   };
 
-  const playSample = (id: number) => {
+  const playSample = (id: string, buttonId: string, iconId: string) => {
+    let button = document.getElementById(buttonId);
+    const icon = document.getElementById(iconId);
+    if (icon) icon.style.opacity = "1";
+    if (button) button.style.borderColor = "grey";
+    setTimeout(() => {
+      if (button) button.style.borderColor = "";
+    }, 100);
+
     const audioElem = document.getElementById(
       id.toString(),
     ) as HTMLAudioElement | null;
@@ -45,7 +55,9 @@ export const SampleBundle = () => {
     }
   };
 
-  const pauseSample = (id: number) => {
+  const pauseSample = (id: string, iconId: string) => {
+    const icon = document.getElementById(iconId);
+    if (icon) icon.style.opacity = "0";
     const audioElem = document.getElementById(
       id.toString(),
     ) as HTMLAudioElement | null;
@@ -64,17 +76,33 @@ export const SampleBundle = () => {
       <Card className="w-48 h-48 overflow-hidden flex ">
         <ScrollArea className="h-full text-left mt-2 overflow-hidden ml-[7px]">
           {fileUrls.map((url, i) => {
+            const audioId = uuidv4();
+            const buttonId = `button-${audioId}`;
+            const iconId = `icon-${audioId}`;
             return (
               <Button
                 key={i}
+                id={buttonId}
                 variant="outline"
                 size="icon"
-                onMouseEnter={() => playSample(i)}
-                onClick={() => playSample(i)}
-                onMouseLeave={() => pauseSample(i)}
-                className="mx-0.5"
+                onMouseEnter={() => playSample(audioId, buttonId, iconId)}
+                onClick={() => playSample(audioId, buttonId)}
+                onMouseLeave={() => pauseSample(audioId, iconId)}
+                className="m-0.5"
               >
-                <audio id={i.toString()} src={url}></audio>
+                <PlusIcon
+                  className={`h-6
+                  w-6
+                  flex 
+                  items-end 
+                  text-zinc-100/30  
+                  rounded-md 
+                  hover:text-zinc-100 
+                 opacity-0
+                 `}
+                  id={iconId}
+                />
+                <audio id={audioId} src={url}></audio>
               </Button>
             );
           })}
