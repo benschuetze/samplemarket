40f4c99cd184257c17dd2e4c81ca663c175169d6
Benjamin <benschuetze@gmx.de>
2024-04-05 16:12:18 +0200

    added components

--- src/routes/upload.tsx
+++ src/routes/upload.tsx
@@ -1,30 +1,9 @@
-import { createFileRoute} from "@tanstack/react-router";
-  export const Route = createFileRoute("/upload")({
+import { createFileRoute } from "@tanstack/react-router";
+import { UploadPage } from "../components/upload-page";
+export const Route = createFileRoute("/upload")({
   component: Upload,
 });
 
-
-function Upload()  {
-  return (
-  <div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  </div>
-  )
+function Upload() {
+  return <UploadPage />;
 }
